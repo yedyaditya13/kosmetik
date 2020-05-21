c41f320d9472bb212f0967a5a4bf19a809b22a9a
yedyaditya <yedyaditya@gmail.com>
2020-05-21 07:36:07 +0700

    first admin structur project

---
+++ admin/includes/scripts.php
@@ -0,0 +1,8 @@
+<!-- jQuery 3 -->
+<script src="../bower_components/jquery/dist/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="../bower_components/jquery-ui/jquery-ui.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../dist/js/adminlte.min.js"></script>
