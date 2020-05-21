ab0c26c6bc08cd19845978f8e9b35497da939a39
yedyaditya <yedyaditya@gmail.com>
2020-05-21 08:00:22 +0700

    create ui on dashboard

--- admin/home.php
+++ admin/home.php
@@ -60,13 +60,24 @@
           <!-- small box -->
           <div class="small-box bg-aqua">
             <div class="inner">
+              <?php
+                  $stmt = $conn->prepare("SELECT * FROM details LEFT JOIN products ON products.id=details.product_id");
+                  $stmt->execute();
 
-            <p>Total Sales</p>
+                  $total = 0;
+                  foreach($stmt as $srow){
+                    $subtotal = $srow['price']*$srow['quantity'];
+                    $total += $subtotal;
+                  }
+
+                  echo "<h3>Rp ".number_format($total, 2, ',', '.')."</h3>";
+                ?>
+            <p>Total Penjualan</p>
             </div>
             <div class="icon">
               <i class="fa fa-shopping-cart"></i>
             </div>
-            <a href="book.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <!-- <a href="book.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
           </div>
         </div>
         <!-- ./col -->
@@ -74,14 +85,20 @@
           <!-- small box -->
           <div class="small-box bg-green">
             <div class="inner">
-             
-          
+              <?php
+                  $stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows FROM products");
+                  $stmt->execute();
+                  $prow =  $stmt->fetch();
+
+                  echo "<h3>".$prow['numrows']."</h3>";
+                ?>
+
               <p>Number of Products</p>
             </div>
             <div class="icon">
               <i class="fa fa-barcode"></i>
             </div>
-            <a href="student.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <!-- <a href="student.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
           </div>
         </div>
         <!-- ./col -->
@@ -89,14 +106,20 @@
           <!-- small box -->
           <div class="small-box bg-yellow">
             <div class="inner">
-              
-             
+              <?php
+                  $stmt = $conn->prepare("SELECT *, COUNT(*) AS numrows FROM users");
+                  $stmt->execute();
+                  $urow =  $stmt->fetch();
+
+                  echo "<h3>".$urow['numrows']."</h3>";
+                ?>
+
               <p>Number of Users</p>
             </div>
             <div class="icon">
               <i class="fa fa-users"></i>
             </div>
-            <a href="return.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <!-- <a href="return.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
           </div>
         </div>
         <!-- ./col -->
@@ -104,13 +127,25 @@
           <!-- small box -->
           <div class="small-box bg-red">
             <div class="inner">
-              
-              <p>Sales Today</p>
+              <?php
+                  $stmt = $conn->prepare("SELECT * FROM details LEFT JOIN sales ON sales.id=details.sales_id LEFT JOIN products ON products.id=details.product_id WHERE sales_date=:sales_date");
+                  $stmt->execute(['sales_date'=>$today]);
+
+                  $total = 0;
+                  foreach($stmt as $trow){
+                    $subtotal = $trow['price']*$trow['quantity'];
+                    $total += $subtotal;
+                  }
+
+                  echo "<h3>&#36; ".number_format($total, 2, ',', '.')."</h3>";
+                  
+                ?>
+              <p>Penjualan Hari Ini</p>
             </div>
             <div class="icon">
               <i class="fa fa-money"></i>
             </div>
-            <a href="borrow.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <!-- <a href="borrow.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> -->
           </div>
         </div>
         <!-- ./col -->
